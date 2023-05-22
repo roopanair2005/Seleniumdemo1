575a2a64d9abfbc0a7c04c8c8b82b136fd6fffaa
roopa <roopanair2005@gmail.com>
2023-05-22 15:25:15 +0530

    Printing message

---
+++ seleniumdemo/src/Pack200/democlass.java
@@ -0,0 +1,10 @@
+package Pack200;
+
+public class democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello friends...Going to work with Github...");
+	}
+
+}
